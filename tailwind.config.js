c156fe3a72287d20af6c95db814c4acb36db09b3
Aryan <aryankg071@gmail.com>
2026-01-21 15:34:14 +0530

    Pushed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html"],
+  content: [
+    "./templates/**/*.html",
+    "./static/**/*.js",
+    "./**/*.html"
+  ],
   theme: {
     extend: {},
   },
